4b2ccde36bdd35d85e5bc397a512baa8701b226b
Paulo Henrique <phvcandido@gmail.com>
2021-11-11 09:01:49 -0300

    ✨feat: Estrutura de conversão funcional

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React from "react";
 import { Typography, Container } from "@material-ui/core";
 import MUIEditor, { MUIEditorState, toHTML } from "react-mui-draft-wysiwyg";
+import { convertFromHTML, ContentState } from "draft-js";
 
 export default function App() {
+  const editorConfig = {};
+  const [html, setHTML] = React.useState(
+    '<p>TEste de <span style="font-weight: bold;">Bold</span></p>'
+  );
   const [editorState, setEditorState] = React.useState(
     MUIEditorState.createEmpty()
   );
 
   const onChange = (val) => {
     setEditorState(val);
+    setHTML(toHTML(val.getCurrentContent()));
   };
+
+  const text = '<p>TEste de <span style="font-weight: bold;">Bold</span></p>';
+  React.useEffect(() => {
+    const convertedHTML = convertFromHTML(text);
+    const convertedHTMLContent = ContentState.createFromBlockArray(
+      convertedHTML.contentBlocks,
+      convertedHTML.entityMap
+    );
+    setEditorState(
+      MUIEditorState.createWithContent(editorConfig, convertedHTMLContent)
+    );
+  }, []);
 
   return (
     <div className="App">
       <Container>
         <Typography variant="h6">Editor padrão Text to HTML </Typography>
         <MUIEditor editorState={editorState} onChange={onChange} />
-        <pre>{JSON.stringify(editorState, null, 2)}</pre>
         <pre>
           {JSON.stringify(toHTML(editorState.getCurrentContent()), null, 2)}
         </pre>
+        <div dangerouslySetInnerHTML={{ __html: html }}></div>
       </Container>
     </div>
   );
